a369d913f7e32ff9a933a158ceeb1f30c39e1a11
Grandeur77 <fernandoshamen77@gmail.com>
2025-05-13 16:12:31 +0530

    Make changes in Navbar

--- fixmate-frontend/src/components/Navbar.jsx
+++ fixmate-frontend/src/components/Navbar.jsx
@@ -195,13 +195,13 @@ const Navbar = () => {
             <>
               <Link
                 to="/login"
-                className="bg-white text-orange-500 py-2 px-4 rounded-full font-semibold hover:bg-gray-200"
+                className="bg-white text-blue-900 py-2 px-4 rounded-full font-semibold hover:bg-gray-200"
               >
                 Login
               </Link>
               <Link
                 to="/signup"
-                className="border border-white text-white py-2 px-4 rounded-full font-semibold hover:bg-white hover:text-orange-500"
+                className="border border-white text-white py-2 px-4 rounded-full font-semibold hover:bg-white hover:text-blue-900"
               >
                 Join
               </Link>
@@ -218,7 +218,7 @@ const Navbar = () => {
               placeholder="Search here..."
               value={searchQuery}
               onChange={handleInputChange}
-              className="w-full py-3 px-6 pr-14 rounded-full border-none shadow-sm placeholder-gray-400 focus:ring-2 focus:ring-orange-500 focus:outline-none"
+              className="w-full py-3 px-6 pr-14 rounded-full border-none shadow-sm placeholder-gray-400 focus:ring-2 focus:ring-blue-500 focus:outline-none"
               style={{ backgroundColor: '#FFF' }}
             />
             <button
